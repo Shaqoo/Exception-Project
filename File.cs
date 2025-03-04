7b77b53c1f10f090ba496fa86a5634add8aeff72
Shaqoo <ShakkirullahOhio@gmail.com>
2025-03-04 10:43:13 +0100

    another commit

---
+++ File.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+namespace asd
+{
+    public class File
+    {
+    //   public void NewFile(string name)
+    //   {
+    //     try
+    //     {
+    //         Console.WriteLine($"Create a file named {name}\n.................\n");
+    //         using(FileStream file = File. )
+    //     }
+    //   }   
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// try
+// {
+// using(FileStream fileStream = File.Create("Shaqo"))
+// {
+//     Console.WriteLine("It has been created successfully");
+// }
+// }
+// catch(Exception MyExcep)
+// {
+//     Console.WriteLine(MyExcep.ToString());
+// }
+// if (File.Exists("Shaqo.txt"))
+// {
+//     File.Delete("Shaqo.txt");
+//     using(FileStream file = File.Create("Books"))
+//     {
+//         Console.WriteLine("It has been created successfully");
+//     }
+//     using(StreamWriter streamWriter = File.CreateText("Books.txt"))
+//     {
+//         streamWriter.WriteLine("I am a boy");
+//         streamWriter.WriteLine("I go to school on a regular basis");
+//         streamWriter.WriteLine("I am a muslim");
+//     }
+//     string[] myArray = File.ReadAllLines("Books.txt");
+//     foreach (string item in myArray)
+//     {
+//      Console.WriteLine(item);   
+//     }
+// }
+static void Exception()
+{
+    Exception e = new Exception("Wrong input");
+    throw e;
+}
+Exception();
+
+int m = 8;
